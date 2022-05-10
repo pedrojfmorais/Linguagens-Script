aa6bb038410da35c84007b3811d4b876184faad2
pedrojfmorais <pedrojfmorais@hotmail.com>
2022-05-11 00:01:33 +0100

    correção ficha 8

--- ficha8/src/components/card/card.component.jsx
+++ ficha8/src/components/card/card.component.jsx
@@ -4,9 +4,9 @@ import { PLACEHOLDER_CARDBACK_PATH, PLACEHOLDER_CARD_PATH } from "../../constant
 
 const Card = props => {
     return (
-        <div className="card" data-logo="angular">
-            <img src={PLACEHOLDER_CARDBACK_PATH} className="card-back" />
-            <img src={PLACEHOLDER_CARD_PATH + props.name + ".png"} className="card-front" />
+        <div className="card" data-logo={props.name}>
+            <img src={PLACEHOLDER_CARDBACK_PATH} alt={props.name} className="card-back" />
+            <img src={PLACEHOLDER_CARD_PATH + props.name + ".png"} alt={props.name} className="card-front" />
         </div>
     );
 }
